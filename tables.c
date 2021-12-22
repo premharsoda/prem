5161116d762327662c4af84c02b611eaaae5ef52
premharsoda <96510863+premharsoda@users.noreply.github.com>
2021-12-22 10:39:23 +0530

    Add files via upload

---
+++ tables.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int num;
+    printf("Enter the value of number");
+    scanf("%d",&num);
+    printf("%o\n",num);
+    printf("%x\n",num);
+
+
+}
